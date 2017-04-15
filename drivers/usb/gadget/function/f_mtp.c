85d22082fb00a7bc3a66e9aa8a56b0d84d109625
zhaorui <rio.zhao@oneplus.net>
2017-04-15 02:06:49 -0700

    [16859][BSP][Perf/Power]:Speed up TMP data transfer rate
    Plan: 优化代码逻辑，在文件写入时disable LPM_sleep
    Type: PATCH
    Test: MTP文件写入
    
    Change-Id: I68b82b14404fae2de6c89a03933ffb8af1a87a84
    Signed-off-by: zhaorui <rio.zhao@oneplus.net>
    (cherry picked from commit 5be1973ca58efb3d729830446adfbfaff4b7796e)

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -82,9 +82,9 @@
 
 #define MAX_ITERATION		100
 #ifdef VENDOR_EDIT
-/* values for qos requests */
-#define FILE_LENGTH	10 * 1024 * 1024
-#define PM_QOS_TIMEOUT	200000
+#define PM_QOS_TIMEOUT	3000000
+
+static bool mtp_receive_flag;
 #endif
 
 unsigned int mtp_rx_req_len = MTP_RX_BUFFER_INIT_SIZE;
@@ -101,8 +101,6 @@ static const char mtp_shortname[] = DRIVER_NAME "_usb";
 static struct pm_qos_request little_cpu_mtp_freq;
 static struct pm_qos_request devfreq_mtp_request;
 //static struct pm_qos_request big_cpu_mtp_freq;
-static struct delayed_work cpu_freq_qos_work;
-static struct workqueue_struct *cpu_freq_qos_queue;
 #endif
 
 struct mtp_dev {
@@ -974,7 +972,7 @@ static void receive_file_work(struct work_struct *data)
 		DBG(cdev, "%s- count(%lld) not multiple of mtu(%d)\n", __func__,
 						count, dev->ep_out->maxpacket);
 
-#ifdef VENDOR_EDIT
+#if 0
 	if (delayed_work_pending(&cpu_freq_qos_work))
 		cancel_delayed_work(&cpu_freq_qos_work);
 	pm_qos_update_request(&little_cpu_mtp_freq, MAX_CPUFREQ);
@@ -1081,23 +1079,11 @@ static void receive_file_work(struct work_struct *data)
 		}
 	}
 
-#ifdef VENDOR_EDIT
-	queue_delayed_work(cpu_freq_qos_queue, &cpu_freq_qos_work, msecs_to_jiffies(1000)*3);
-#endif
 	DBG(cdev, "receive_file_work returning %d\n", r);
 	/* write the result */
 	dev->xfer_result = r;
 	smp_wmb();
 }
-
-#ifdef VENDOR_EDIT
-static void update_qos_request(struct work_struct *data)
-{
-	pm_qos_update_request(&little_cpu_mtp_freq, MIN_CPUFREQ);
-	pm_qos_update_request(&devfreq_mtp_request, MIN_CPUFREQ);
-	//pm_qos_update_request(&big_cpu_mtp_freq, MIN_CPUFREQ);
-}
-#endif
 
 static int mtp_send_event(struct mtp_dev *dev, struct mtp_event *event)
 {
@@ -1183,15 +1169,35 @@ static long mtp_send_receive_ioctl(struct file *fp, unsigned code,
 		dev->xfer_send_header = 0;
 	} else {
 		work = &dev->receive_file_work;
+#ifdef VENDOR_EDIT
+		pm_qos_update_request(&devfreq_mtp_request, MAX_CPUFREQ);
+		pm_qos_update_request(&little_cpu_mtp_freq, MAX_CPUFREQ);
+		msm_cpuidle_set_sleep_disable(true);
+		mtp_receive_flag = true;
+#endif
 	}
 
 	/* We do the file transfer on a work queue so it will run
 	 * in kernel context, which is necessary for vfs_read and
 	 * vfs_write to use our buffers in the kernel address space.
 	 */
+#ifdef VENDOR_EDIT
 	queue_work(dev->wq, work);
 	/* wait for operation to complete */
 	flush_workqueue(dev->wq);
+	if (mtp_receive_flag) {
+		mtp_receive_flag = false;
+		pm_qos_update_request_timeout(&devfreq_mtp_request,
+					MAX_CPUFREQ, PM_QOS_TIMEOUT);
+		pm_qos_update_request_timeout(&little_cpu_mtp_freq,
+					MAX_CPUFREQ, PM_QOS_TIMEOUT);
+		msm_cpuidle_set_sleep_disable(false);
+	}
+#else
+	queue_work(dev->wq, work);
+	/* wait for operation to complete */
+	flush_workqueue(dev->wq);
+#endif
 	fput(filp);
 
 	/* read the result */
@@ -1338,6 +1344,12 @@ static int mtp_release(struct inode *ip, struct file *fp)
 {
 	printk(KERN_INFO "mtp_release\n");
 
+#ifdef VENDOR_EDIT
+	if (mtp_receive_flag) {
+		mtp_receive_flag = false;
+		msm_cpuidle_set_sleep_disable(false);
+	}
+#endif
 	mtp_unlock(&_mtp_dev->open_excl);
 	return 0;
 }
@@ -1774,8 +1786,6 @@ static int __mtp_setup(struct mtp_instance *fi_mtp)
 	INIT_WORK(&dev->receive_file_work, receive_file_work);
 
 #ifdef VENDOR_EDIT
-	cpu_freq_qos_queue = create_singlethread_workqueue("f_mtp_qos");
-	INIT_DELAYED_WORK(&cpu_freq_qos_work, update_qos_request);
 	pm_qos_add_request(&little_cpu_mtp_freq, PM_QOS_C0_CPUFREQ_MIN, MIN_CPUFREQ);
 	pm_qos_add_request(&devfreq_mtp_request, PM_QOS_DEVFREQ_MIN, MIN_CPUFREQ);
 	//pm_qos_add_request(&big_cpu_mtp_freq, PM_QOS_C1_CPUFREQ_MIN, MIN_CPUFREQ);
@@ -1794,7 +1804,6 @@ err2:
 	//pm_qos_remove_request(&big_cpu_mtp_freq);
 	pm_qos_remove_request(&little_cpu_mtp_freq);
 	pm_qos_remove_request(&devfreq_mtp_request);
-	destroy_workqueue(cpu_freq_qos_queue);
 #endif
 	destroy_workqueue(dev->wq);
 err1:
@@ -1822,7 +1831,6 @@ static void mtp_cleanup(void)
 	//pm_qos_remove_request(&big_cpu_mtp_freq);
 	pm_qos_remove_request(&little_cpu_mtp_freq);
 	pm_qos_remove_request(&devfreq_mtp_request);
-	destroy_workqueue(cpu_freq_qos_queue);
 #endif
 	misc_deregister(&mtp_device);
 	destroy_workqueue(dev->wq);
